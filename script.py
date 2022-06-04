3a1d3dd3e66d3105d32d48cfa698ffa658267735
alexdietz1988 <alex.dietz@gmail.com>
2022-06-04 13:43:25 -0400

    Combine materials from laptop work session

--- script.py
+++ script.py
@@ -1,28 +1,25 @@
 import csv
 
-filename = 'data.csv'
-
-def format_artist(string):
-    commaIndex = string.find(',')
-    if commaIndex > 0:
-        return f'{string[commaIndex + 2:]} {string[:commaIndex]}'
+filename='data.csv'
 
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
     for index, column_header in enumerate(header_row):
-        print(index, column_header)
+        print(index,column_header)
 
-    string = ''
-
+    artists, titles = [], []
     for row in reader:
+
+        artist = row[3]
+        commaIndex = artist.find(',')
+        if commaIndex > 0:
+            artist = f'{artist[commaIndex + 2:]} {artist[:commaIndex]}'
+        artists.append(artist)
+
         title = row[4]
-        artist = format_artist(row[3])
-        image = ''
-        year = row[5]
-        rank = row[2]
+        titles.append(title)
 
-        string += f'INSERT INTO main_app_album(title, artist, image, rank, year) VALUES("{title}", "{artist}", {year}, {rank});'
-
-    print(string)
+print(artists)
+print(titles)
