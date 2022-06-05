243815406f0b989f12d7cdb3d78285a1974e3160
alexdietz1988 <alex.dietz@gmail.com>
2022-06-05 08:13:47 -0400

    Just making sure GitHub is up-to-date with yesterday's work

--- script.py
+++ script.py
@@ -1,28 +1,38 @@
 import csv
 
-filename = 'data.csv'
+filename='data.csv'
 
-def format_artist(string):
-    commaIndex = string.find(',')
-    if commaIndex > 0:
-        return f'{string[commaIndex + 2:]} {string[:commaIndex]}'
+seed = ''
 
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
     for index, column_header in enumerate(header_row):
-        print(index, column_header)
-
-    string = ''
+        print(index,column_header)
 
     for row in reader:
-        title = row[4]
-        artist = format_artist(row[3])
-        image = ''
+
+        artist = row[3].replace("'","''")
+        commaIndex = artist.find(',')
+        if commaIndex > 0:
+            artist = f'{artist[commaIndex + 2:]} {artist[:commaIndex]}'
+
+        title = row[4].replace("'","''")
         year = row[5]
-        rank = row[2]
 
-        string += f'INSERT INTO main_app_album(title, artist, image, rank, year) VALUES("{title}", "{artist}", {year}, {rank});'
+        rank_2003 = row[0]
+        rank_2012 = row[1]
+        rank_2020 = row[2]
+        if rank_2003 == '': rank_2003 = 0
+        if rank_2012 == '': rank_2012 = 0
+        if rank_2020 == '': rank_2020 = 0
 
-    print(string)
+        image = f'http://alexdietz.com/covers/{rank_2020}.jpg'
+
+        seed += f'INSERT INTO main_app_album(title, artist, image, rank_2003, rank_2012, rank_2020, year)\n\tVALUES\n\t\t('
+        seed += f"'{title}', '{artist}', '{image}', {rank_2003}, {rank_2012}, {rank_2020}, {year});\n\n"
+
+text_file = open("seed.txt", "w")
+n = text_file.write(seed)
+text_file.close()
