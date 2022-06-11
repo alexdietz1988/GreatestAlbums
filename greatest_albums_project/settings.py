42e840591313f59f5d2f4e767980677d9110c6bf
alexdietz1988 <alex.dietz@gmail.com>
2022-06-11 09:31:07 -0400

    Successfully redeployed

--- greatest_albums_project/settings.py
+++ greatest_albums_project/settings.py
@@ -130,5 +130,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-import django_on_heroku
-django_on_heroku.settings(locals())
+# import django_on_heroku
+# django_on_heroku.settings(locals())
